f630225da5bb21790cfc189c1c34c3ec2d3b33a8
Tanner Willis <tannerwillis49@gmail.com>
2025-02-06 13:03:24 -0600

    Simple RNG using a PCG (supports uint32 and 0-1 floats)

---
+++ Source/Luft/Random.hpp
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include "Base.hpp"
+
+struct PcgRandomContext
+{
+	uint32 State;
+};
+
+inline uint32 RandomPcg(PcgRandomContext* context)
+{
+	const uint32 state = context->State;
+	context->State = context->State * 747796405U + 2891336453U;
+	const uint32 word = ((state >> ((state >> 28U) + 4U)) ^ state) * 277803737U;
+	return (word >> 22U) ^ word;
+}
+
+inline PcgRandomContext SeedRandomPcg(uint32 initialState)
+{
+	PcgRandomContext context =
+	{
+		.State = initialState,
+	};
+	RandomPcg(&context);
+	return context;
+}
+
+class RandomContext
+{
+public:
+	explicit RandomContext(uint32 seed)
+		: Context(SeedRandomPcg(seed))
+	{
+	}
+
+	uint32 Uint32()
+	{
+		return RandomPcg(&Context);
+	}
+
+	float Float01()
+	{
+		return static_cast<float>(Uint32()) / static_cast<float>(0xFFFFFFFF);
+	}
+
+private:
+	PcgRandomContext Context;
+};
